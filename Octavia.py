724c2d84eceda507235cb5b6e6be09fb017e3d9b
Bass Shadow <jorgebarrios@email.arizona.edu>
2018-11-16 23:39:29 -0700

    Our data folder holding all midi files translated into notes along with the main structure of our network to be modified later

--- Octavia.py
+++ Octavia.py
@@ -5,40 +5,54 @@ Octavia, musical generation A.I.
     Jorge Barrios (Sisyphus Igor)
 '''
 
-#importing required dependancies
-#import keras #main library for neural networks
-import numpy as np #mathematical structures, primarily arrays
-import matplotlib.pyplot as plt #optional for visualisation 
-import glob #used for parsing data from folders
-import music21 as m21 #needed for parsing information from MIDI files into useful formats
+""" This module prepares midi file data and feeds it to the neural
+    network for training """
+import glob
 import pickle
+import numpy
+from music21 import converter, instrument, note, chord
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
 
+def train_network():
+    """ Train a Neural Network to generate music """
+    notes = get_notes()
 
-#Data prep
-#This code was extrapolated from https://github.com/Skuldur/Classical-Piano-Composer/blob/master/lstm.py, written by Skuldur
-def get_notes(folder):
-    ''' 
-    This, for now until I can more easily manuver the music21 package, takes all the notes from the "Raw_Files" folder
-    '''
+    # get amount of pitch names
+    n_vocab = len(set(notes))
+
+    network_input, network_output = prepare_sequences(notes, n_vocab)
+
+    model = create_network(network_input, n_vocab)
+
+    train(model, network_input, network_output)
+
+def get_notes():
+    """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
 
-    for file in glob.glob(folder + "/*.mid"):
-        midi = m21.converter.parse(file)
+    for file in glob.glob("Raw_Files/*.mid"):
+        midi = converter.parse(file)
 
         print("Parsing %s" % file)
 
         notes_to_parse = None
 
         try: # file has instrument parts
-            s2 = m21.instrument.partitionByInstrument(midi)
+            s2 = instrument.partitionByInstrument(midi)
             notes_to_parse = s2.parts[0].recurse() 
         except: # file has notes in a flat structure
             notes_to_parse = midi.flat.notes
 
         for element in notes_to_parse:
-            if isinstance(element, m21.note.Note):
+            if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
-            elif isinstance(element, m21.chord.Chord):
+            elif isinstance(element, chord.Chord):
                 notes.append('.'.join(str(n) for n in element.normalOrder))
 
     with open('data/notes', 'wb') as filepath:
@@ -46,9 +60,70 @@ def get_notes(folder):
 
     return notes
 
-schist = get_notes("RawFiles")
+def prepare_sequences(notes, n_vocab):
+    """ Prepare the sequences used by the Neural Network """
+    sequence_length = 100
 
-pitchnames = sorted(set(item for item in schist))
-print(pitchnames)
-print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-print(schist)
+    # get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+
+     # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    network_input = []
+    network_output = []
+
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # reshape the input into a format compatible with LSTM layers
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+    # normalize input
+    network_input = network_input / float(n_vocab)
+
+    network_output = np_utils.to_categorical(network_output)
+
+    return (network_input, network_output)
+
+def create_network(network_input, n_vocab):
+    """ create the structure of the neural network """
+    model = Sequential()
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        return_sequences=True
+    ))
+    model.add(Dropout(0.3))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(512))
+    model.add(Dense(256))
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    return model
+
+def train(model, network_input, network_output):
+    """ train the neural network """
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    callbacks_list = [checkpoint]
+
+    model.fit(network_input, network_output, epochs=5, batch_size=64, callbacks=callbacks_list)
+
+
+train_network()
